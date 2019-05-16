e3dc83ce9544e5cc5c1aca8bf73840b2c961f4f2
Glenn Antoine <me@glennantoine.com>
2019-05-16 11:33:43 -0400

    base project

---
+++ App.js
@@ -0,0 +1,50 @@
+/**
+ * OneAmerica Sentiment App
+ * Glenn Antoine 05/2019
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import Emoji from 'react-native-emoji';
+
+type Props = {};
+
+export default class App extends Component<Props> {
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Morning Coffee & React Native: </Text><Emoji name="coffee" style={{fontSize: 60}} />
+        <Emoji name="smiley" style={styles.icon} />
+        <Emoji name="neutral_face" style={styles.icon} />
+        <Emoji name="frowning" style={styles.icon} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  icon: {
+    fontSize: 90,
+    marginBottom: 15,
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
---
+++ README.md
@@ -0,0 +1,13 @@
+
+In researching a number of details around the creation of React Native apps there were several resources that stood out and as such I wanted to record them here.
+
+
+Creating Icons for IOS Apps:
+https://medium.com/@scottianstewart/react-native-add-app-icons-and-launch-screens-onto-ios-and-android-apps-3bfbc20b7d4c
+
+
+React Native Emoji
+https://github.com/EricPKerr/react-native-emoji
+
+Emoji Catalog
+https://unicodey.com/emoji-data/table.htm
